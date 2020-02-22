438091ec12dafbbaccd617c80348b1e55d78d5cf
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2020-02-22 23:51:46 +0900

    화면 전환

--- app/src/main/java/com/shindorim/bbibbi/MainActivity.java
+++ app/src/main/java/com/shindorim/bbibbi/MainActivity.java
@@ -1,8 +1,14 @@
 package com.shindorim.bbibbi;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendBtn = findViewById(R.id.sendBtn);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(getApplicationContext(),"안녕", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MainActivity.this, dictionary.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/shindorim/bbibbi/dictionary.java
@@ -0,0 +1,15 @@
+package com.shindorim.bbibbi;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dictionary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+    }
+}
